16e826683bb278c103887909f2075c7872b82ec3
Cynthia Lee <cynthia.lee335@gmail.com>
2019-11-15 20:26:26 -0500

    Added my firebase config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,6 +5,15 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
+    apiKey: "AIzaSyAAj3gbzZNr3Yuz2xJQE-rPKyaQcXi5lkg",
+    authDomain: "todo-rrf-316-cyllee.firebaseapp.com",
+    databaseURL: "https://todo-rrf-316-cyllee.firebaseio.com",
+    projectId: "todo-rrf-316-cyllee",
+    storageBucket: "todo-rrf-316-cyllee.appspot.com",
+    messagingSenderId: "310472154444",
+    appId: "1:310472154444:web:3c6b5bf103de9f034a03e9",
+    measurementId: "G-V56WNREQQW"
+    /*
     apiKey: "AIzaSyCJxkqx-6PMJrZ7ACkrgbO55b5wmJdop1Y",
     authDomain: "todo-rrf-316.firebaseapp.com",
     databaseURL: "https://todo-rrf-316.firebaseio.com",
@@ -13,6 +22,7 @@ var firebaseConfig = {
     messagingSenderId: "892398996038",
     appId: "1:892398996038:web:1fb9157fc6c5d266e01847",
     measurementId: "G-TEGQB3MZ23"
+    */
 };
 firebase.initializeApp(firebaseConfig);
 
